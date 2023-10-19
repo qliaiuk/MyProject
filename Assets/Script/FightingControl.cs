a253c65a12c136f9e22eec3c2bb7d6c974957b3d
qliaiuk <835647922@qq.com>
2023-10-19 15:14:44 +0800

    update

--- Assets/Script/FightingControl.cs
+++ Assets/Script/FightingControl.cs
@@ -11,7 +11,10 @@ public class FightingControl : MonoBehaviour
     public RoleInfo roleInfo; //角色数据
 
 
-    
+    // 战斗实现逻辑
+    // 由于战斗为全自动进行，因此可以直接计算出战斗结果，实现“跳过战斗”功能
+    // 在快速计算完战斗结果时，返回一个存储战斗过程的变量
+    // 实际玩家观看的战斗，为“播放”这个存储战斗过程的结果
 
     // Start is called before the first frame update
     void Start()
